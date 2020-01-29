223099f0665da37a39b022613485f09db34c5d53
Mario <mario.saliba98@gmail.com>
2020-01-29 22:36:16 +0200

    init

---
+++ src/controllers/auth.js
@@ -0,0 +1,63 @@
+const { findByUsername, addNewUser } = require('../models/users/User.model');
+const bcrypt = require('bcrypt');
+
+exports.loginPage = (req, res) => {
+  res.render('login', { activePage: { login: true } });
+};
+exports.registerPage = (req, res) => {
+  res.render('register', { activePage: { register: true } });
+};
+
+exports.authenticate = async (req, res) => {
+  try {
+    //   send back the user that the user does not exist
+    // or user has not been found
+    const user = await findByUsername(req.body.username);
+
+    bcrypt.compare(req.body.password, user.password, function(err, result) {
+      if (!result) {
+        return res.render('login', {
+          activePage: { login: true },
+          error: 'Password is incorrect'
+        });
+      }
+
+      res.render('home', {
+        activePage: { home: true },
+        signedIn: true,
+        username: user.username
+      });
+
+    });
+  } catch (e) {
+    res.render('login', {
+      activePage: { login: true },
+      error: e.message
+    });
+  }
+};
+
+exports.addUser = (req, res, err) => {
+  const { username, password, confirmPassword } = req.body;
+
+  if (password !== confirmPassword) {
+    return res.render('register', {
+      activePage: { register: true },
+      error: 'Passwords do not match'
+    });
+  }
+
+  bcrypt.hash(password, 10, async (err, hash) => {
+    if (err) next(err);
+    try {
+      await addNewUser(username, hash);
+
+      res.redirect('/login');
+    } catch (error) {
+      res.render('register', {
+        activePage: { register: true },
+        error: error.message
+      });
+    }
+  });
+};
---
+++ walkthrough/step01.md
@@ -0,0 +1,19 @@
+# Step 1 - Verify a user
+
+<!-- intro to bcrypt -->
+
+<!-- dangers of plaintext -->
+
+<!-- login - get password, verify hashed password -->
+
+---
+## Commit your changes
+
+```bash
+git add .
+git commit -m 'enter relevant message'
+```
+
+## [**next step >>>**](walkthrough/step01.md)
+
+<!-- useful links -->
