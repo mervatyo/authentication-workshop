0aab9d3529fef4d7785290563d98e0ab0257b2aa
Mario <mario.saliba98@gmail.com>
2020-02-06 13:05:39 +0200

    adds the verify step

--- src/controllers/home.js
+++ src/controllers/home.js
@@ -1,10 +1,23 @@
 exports.get = (req, res) => {
+    
     if (req.cookies.access_token) {
-        return res.render('home', {
-            activePage: { home: true },
-            signedIn: true,
-            username: req.cookies.access_token
-        })
+
+        jwt.verify(req.cookies.access_token, process.env.JWT_SECRET, function(err, decoded) {
+
+            if (error) {
+                return res.render('home', {
+                    activePage: { home: true },
+                    error: err.message
+                })
+            }
+
+            return res.render('home', {
+                activePage: { home: true },
+                signedIn: true,
+                username: decoded
+            })
+        });
+
     }
 
     res.render('home', { activePage: { home: true } });
