ad4d026444e3934c9c49281db952f55a0bd5ad41
Mario <mario.saliba98@gmail.com>
2020-02-06 14:51:27 +0200

    finish the step04 solution

--- src/controllers/home.js
+++ src/controllers/home.js
@@ -1,24 +1,25 @@
+const jwt = require('jsonwebtoken');
+
 exports.get = (req, res) => {
-    
-    if (req.cookies.access_token) {
-
-        jwt.verify(req.cookies.access_token, process.env.JWT_SECRET, function(err, decoded) {
-
-            if (error) {
-                return res.render('home', {
-                    activePage: { home: true },
-                    error: err.message
-                })
-            }
-
-            return res.render('home', {
-                activePage: { home: true },
-                signedIn: true,
-                username: decoded
-            })
+  if (req.cookies.access_token) {
+    jwt.verify(req.cookies.access_token, process.env.JWT_SECRET, function(
+      err,
+      decoded
+    ) {
+      if (err) {
+        return res.render('home', {
+          activePage: { home: true },
+          error: err.message
         });
+      }
 
-    }
-
+      return res.render('home', {
+        activePage: { home: true },
+        signedIn: true,
+        username: decoded
+      });
+    });
+  } else {
     res.render('home', { activePage: { home: true } });
+  }
 };
