4accf6d8de118420c5a4144daf925f834dc46e87
mervat <mervat.yosef.14@gmail.com>
2020-08-23 12:28:28 +0300

    addnewuser

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,33 +1,49 @@
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+const someOtherPlaintextPassword = "";
+
 // use these functions to manipulate our database
-const { findByUsername, addNewUser } = require('../models/users/User.model');
+
+const { findByUsername, addNewUser } = require("../models/users/User.model");
 
 exports.loginPage = (req, res) => {
-  res.render('login', { activePage: { login: true } });
+  res.render("login", { activePage: { login: true } });
 };
 exports.registerPage = (req, res) => {
-  res.render('register', { activePage: { register: true } });
+  res.render("register", { activePage: { register: true } });
 };
 
 // This function handles the POST /addUser route
-// checks if the password and confirmPassword are equal if not send back 
+// checks if the password and confirmPassword are equal if not send back
 // a proper error message
 // hash the password, then add the new user to our database using the v addNewUser method
 // make sure to handle any error that might occured
 exports.addUser = (req, res, err) => {
-  
+  if (req.body.password !== req.body.confirmPassword)
+    return res.render("register", { error: "passwords don't match" });
+  const saltRounds = 10;
+  const myPlaintextPassword = req.body.password;
+  bcrypt.genSalt(saltRounds, function (err, salt) {
+    if (err) return res.render("register", { error: err.message });
+    bcrypt.hash(myPlaintextPassword, salt, function (err, hash) {
+      if (err) return res.render("register", { error: err.message });
+      addNewUser(req.body.username, hash)
+        .then(() => {
+          res.render("home");
+        })
+        .catch((error) => {
+          res.render("register", { error: error.message });
+        });
+    });
+  });
 };
 
 // this function handles the POST /authenticate route
 // it finds the user in our database by his username that he inputed
 // then compares the password that he inputed with the one in the db
-// using bcrypt and then redirects back to the home page 
+// using bcrypt and then redirects back to the home page
 // make sure to look at home.hbs file to be able to modify the home page when user is logged in
 // also handle all possible errors that might occured by sending a message back to the cleint
-exports.authenticate = (req, res) => {
+exports.authenticate = (req, res) => {};
 
-};
-
-
-exports.logout = (req, res) => {
-  
-}
+exports.logout = (req, res) => {};
--- src/models/users/User.model.js
+++ src/models/users/User.model.js
@@ -1,5 +1,5 @@
-const fs = require('fs');
-const db = require('../../../db/dbConnection');
+const fs = require("fs");
+const db = require("../../../db/dbConnection");
 
 // !! will turn the value into a boolean
 // so if length is 0 then it's converted to false which means user not found
@@ -12,17 +12,17 @@ const checkIfUserExists = (username) =>
 exports.findByUsername = (username) =>
   new Promise((resolve, reject) =>
     db
-      .query('SELECT * FROM users WHERE username = $1', username)
+      .query("SELECT * FROM users WHERE username = $1", username)
       .then((user) => {
         if (!user.length) {
-          reject(new Error('No user was found'));
+          reject(new Error("No user was found"));
         }
 
         resolve(user[0]);
       })
       .catch((error) => {
         console.log(`findByUsername Error: ${error}`);
-        reject(new Error('An error has occurred in the db, findByUsername'));
+        reject(new Error("An error has occurred in the db, findByUsername"));
       })
   );
 
@@ -32,24 +32,22 @@ exports.findByUsername = (username) =>
  */
 exports.addNewUser = async (username, password) =>
   new Promise((resolve, reject) =>
-  // EXISTS returns the following [ { exists: BOOLEAN } ]
+    // EXISTS returns the following [ { exists: BOOLEAN } ]
     db
-      .query('SELECT EXISTS(SELECT 1 FROM users WHERE username = $1)', username)
+      .query("SELECT EXISTS(SELECT 1 FROM users WHERE username = $1)", username)
       .then(([{ exists }]) => {
-
         if (exists) {
-          return reject(new Error('User already exists in our database'));
+          return reject(new Error("User already exists in our database"));
         }
 
         // adds the user to the db
-        db.query('INSERT INTO users (username, password) VALUES ($1, $2)', [
+        db.query("INSERT INTO users (username, password) VALUES ($1, $2)", [
           username,
           password,
-        ]).then(() => resolve('User has been added'));
-
+        ]).then(() => resolve("User has been added"));
       })
       .catch((error) => {
         console.log(`addNewUser Error: ${error}`);
-        reject(new Error('An error has occurred in the db, addNewUser'));
+        reject(new Error("An error has occurred in the db, addNewUser"));
       })
   );
